5afdc4dd08790797e4510cb71c29cf3caef31c00
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2024-01-09 10:35:31 +0530

    feat: :sparkles: add sidebar container

---
+++ components/common/sidebar/sidebar.tsx
@@ -0,0 +1,9 @@
+const Sidebar = () => {
+    return (
+        <div className="lg:w-64 w-56 lg:flex hidden h-screen bg-primary-blue fixed top-0 left-0">
+
+        </div>
+    );
+};
+
+export default Sidebar;
