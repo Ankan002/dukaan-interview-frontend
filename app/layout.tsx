9e487bd1018a30a750661b6a855a0da3b256aff4
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2024-01-09 09:41:50 +0530

    feat: :sparkles: add recoil provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { RecoilProvider } from "@/components/providers";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
@@ -16,7 +17,9 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
-			<body className={`${inter.variable}`}>{children}</body>
+			<body className={`${inter.variable}`}>
+				<RecoilProvider>{children}</RecoilProvider>
+			</body>
 		</html>
 	);
 }
